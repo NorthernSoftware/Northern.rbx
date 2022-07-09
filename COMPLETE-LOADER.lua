9dd40ca201a351b9e169875952368924d56329f1
Northern <108973103+NorthernSoftware@users.noreply.github.com>
2022-07-09 10:10:55 +0300

    Update

--- COMPLETE-LOADER.lua
+++ COMPLETE-LOADER.lua
@@ -1,1 +1,2 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/Northern-loader.lua"))()
