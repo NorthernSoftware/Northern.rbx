0904ca900072920596413c6fb3f759305eab7052
1Ronkkeli <108973103+NorthernSoftware@users.noreply.github.com>
2022-07-14 19:55:29 +0300

    Huge changes still have to fix alot

--- Northern-loader.lua
+++
@@ -1,8 +0,0 @@
---Arsenal Undetected
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern-ui/main/source.lua"))()
-if game.PlaceId == 286090429 then
-
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/Games/Arsenal/Arsenal-undetected.lua"))()
-
-end
---
+++ SCRIPT-HUB/Loaders/Northern-loader.lua
@@ -0,0 +1,8 @@
+--Arsenal Undetected
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern-ui/main/source.lua"))()
+if game.PlaceId == 286090429 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/Games/Arsenal/Arsenal-undetected.lua"))()
+
+end
