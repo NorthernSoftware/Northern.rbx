cae20ff82f70c6ce7cc21f9733ce2bd557249e0d
1Ronkkeli <108973103+NorthernSoftware@users.noreply.github.com>
2022-07-14 19:59:26 +0300

    Fixed + Obfuscated ESP

--- SCRIPT-HUB/Loaders/COMPLETE-LOADER.lua
+++ SCRIPT-HUB/Loaders/COMPLETE-LOADER.lua
@@ -1,9 +1,7 @@
---Version 1.1
--- New ui lib for arsenal + updates
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern-ui/main/source.lua"))()
+-- NEW FIXES
+-- STILL HAVE TO FIX FPS
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/Northern-loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/SCRIPT-HUB/Loaders/Northern-loader.lua"))()
 
 
--- SCRIPT-HUB/Loaders/Northern-loader.lua
+++ SCRIPT-HUB/Loaders/Northern-loader.lua
@@ -1,8 +1,9 @@
 --Arsenal Undetected
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern-ui/main/source.lua"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 if game.PlaceId == 286090429 then
 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/Games/Arsenal/Arsenal-undetected.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NorthernSoftware/Northern.rbx/main/SCRIPT-HUB/Games/FPS/Arsenal/Arsenal-undetected.lua"))()
 
 end
